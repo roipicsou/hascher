f1ef5e05edd3cab56b2289e85bf5c68f1bb79c08
roipicsou <pl613330@gmail.com>
2025-01-19 17:22:50 +0100

    toujour pas

--- start.py
+++ start.py
@@ -1,10 +1,4 @@
 import os
-import time
 
-time.sleep(2)
-os.system("mkdir /media/root/disque_dure/")
-os.system("sudo mount /dev/sda1 /media/root/disque_dure/")
-os.system("mkdir /media/root/disque_dure1/")
-os.system("sudo mount /dev/sda2 /media/root/disque_dure1/")
-
+os.system("sudo python3 /media/root/pourquois.py")
 os.system("flask run --host 0.0.0.0 --port 5000")
