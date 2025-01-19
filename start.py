403a96ee6d2cadca143326e504c6f75d937ce297
roipicsou <pl613330@gmail.com>
2025-01-19 16:19:55 +0100

    10

--- start.py
+++ start.py
@@ -1,3 +1,6 @@
 import os
 
+os.system("mkdir /media/root/disque_dure/ && sudo mount /dev/sda1 /media/root/disque_dure/")
+os.system("mkdir /media/root/disque_dure1/ && sudo mount /dev/sda2 /media/root/disque_dure1/")
+
 os.system("flask run --host 0.0.0.0 --port 5000")
