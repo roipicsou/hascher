f2140f60a5c7867ac624afd6184627bfc8e84de3
roipicsou <pl613330@gmail.com>
2025-01-19 16:54:54 +0100

    desolsations

--- start.py
+++ start.py
@@ -2,7 +2,9 @@ import os
 import time
 
 time.sleep(0.5)
-os.system("mkdir /media/root/disque_dure/ && sudo mount /dev/sda1 /media/root/disque_dure/")
-os.system("mkdir /media/root/disque_dure1/ && sudo mount /dev/sda2 /media/root/disque_dure1/")
+os.system("mkdir /media/root/disque_dure/")
+os.system("sudo mount /dev/sda1 /media/root/disque_dure/")
+os.system("mkdir /media/root/disque_dure1/")
+os.system("sudo mount /dev/sda2 /media/root/disque_dure1/")
 
 os.system("flask run --host 0.0.0.0 --port 5000")
