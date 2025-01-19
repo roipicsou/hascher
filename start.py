17d03b702ed192791ee7728235bcdb663d766cf8
roipicsou <pl613330@gmail.com>
2025-01-19 16:30:04 +0100

    desespoire

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 import os
+import time
 
+time.sleep(0.5)
 os.system("mkdir /media/root/disque_dure/ && sudo mount /dev/sda1 /media/root/disque_dure/")
 os.system("mkdir /media/root/disque_dure1/ && sudo mount /dev/sda2 /media/root/disque_dure1/")
 
